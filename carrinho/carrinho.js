0bf8dd37f7a1d6d48236a76e988a360009ab1a85
Ana luiza de sousa silva <correioaninha@gmail.com>
2022-11-23 20:12:00 -0300

    commit cyberpet

--- carrinho/carrinho.js
+++ carrinho/carrinho.js
@@ -1,4 +1,4 @@
-mostrarProdutos = function(dadosProduto){
+mostrarProdutos = function (dadosProduto) {
 
     document.querySelector('#table').innerHTML += `
    
@@ -8,7 +8,7 @@ mostrarProdutos = function(dadosProduto){
                 <img class="card-img-top" src="../home-page/${dadosProduto.imagePath}" alt="Card image cap">                
                 <div class="card-body">
                     <h5 class="card-title">${dadosProduto.nomeProduto}</h5>
-                    <p class="card-text">${dadosProduto.descricao }</p>
+                    <p class="card-text">${dadosProduto.descricao}</p>
                 </div>               
             </div>
         </td>
@@ -24,10 +24,10 @@ mostrarProdutos = function(dadosProduto){
             R$ <label id="precoSubtotal_${dadosProduto.id}"> ${dadosProduto.preco}</label>
         </td>
 </tr>`;
-                 
+
 }
 let produtosCarrinho = [];
-carrega = function(){
+carrega = function () {
 
     let itensCarrinho = getParameterByName("carrinho");
     let itensCarrinhoArray = itensCarrinho.split(',');
@@ -35,32 +35,32 @@ carrega = function(){
 
     let xhr = new XMLHttpRequest();
     let url = "http://localhost:8080/api/produtos/findAll";
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (xhr.readyState == XMLHttpRequest.DONE) {
-     
+
             dadosProdutos = JSON.parse(xhr.responseText);
-            if(dadosProdutos.length> 0){
-                for(let i=0; i<dadosProdutos.length; i++){    
-                    if(itensCarrinhoArray.indexOf(dadosProdutos[i].id.toString()) != -1){
-                        let produtoCarrinho= {idProduto:dadosProdutos[i].id,qtd:1,produto:dadosProdutos[i]};
+            if (dadosProdutos.length > 0) {
+                for (let i = 0; i < dadosProdutos.length; i++) {
+                    if (itensCarrinhoArray.indexOf(dadosProdutos[i].id.toString()) != -1) {
+                        let produtoCarrinho = { idProduto: dadosProdutos[i].id, qtd: 1, produto: dadosProdutos[i] };
                         produtosCarrinho.push(produtoCarrinho);
                         mostrarProdutos(dadosProdutos[i]);
-                    }              
-                   
-                }               
+                    }
+
+                }
             }
-            else{
+            else {
                 alert('Produto não encontrado')
             }
         }
 
 
     }
-    xhr.oneerror=
-    xhr.open('GET',url, true);
+    xhr.oneerror =
+        xhr.open('GET', url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send();
-    
+
 }
 
 getParameterByName = function (name, url = window.location.href) {
@@ -72,33 +72,32 @@ getParameterByName = function (name, url = window.location.href) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-adcItem = function(id){
+adcItem = function (id) {
     let idProduto = parseInt(id.replace("quantItem_", ""));
     let produtoCarrinho = produtosCarrinho.filter(p => p.idProduto == idProduto).shift();
     let valorAtual = document.getElementById("precoSubtotal_" + idProduto).innerHTML;
-    valorAtual = parseFloat(valorAtual)+ produtoCarrinho.produto.preco;
+    valorAtual = parseFloat(valorAtual) + produtoCarrinho.produto.preco;
     document.getElementById("precoSubtotal_" + idProduto).innerHTML = valorAtual;
 
-    document.getElementById(id).innerHTML= parseInt(document.getElementById(id).innerHTML)+1;
+    document.getElementById(id).innerHTML = parseInt(document.getElementById(id).innerHTML) + 1;
 }
 
-retiraItem = function(id){
+retiraItem = function (id) {
     let idProduto = parseInt(id.replace("quantItem_", ""));
     let produtoCarrinho = produtosCarrinho.filter(p => p.idProduto == idProduto).shift();
     let valorAtual = document.getElementById("precoSubtotal_" + idProduto).innerHTML;
-    if(valorAtual > 0){
-        valorAtual = parseFloat(valorAtual)-produtoCarrinho.produto.preco;
+    if (valorAtual > 0) {
+        valorAtual = parseFloat(valorAtual) - produtoCarrinho.produto.preco;
         document.getElementById("precoSubtotal_" + idProduto).innerHTML = valorAtual;
-    
-        document.getElementById(id).innerHTML= parseInt(document.getElementById(id).innerHTML)-1;
-    }
-   
-}
 
-precoItem = function(preco){
+        document.getElementById(id).innerHTML = parseInt(document.getElementById(id).innerHTML) - 1;
+    }
+
+}
+precoItem = function (preco) {
     let precoTotal = 0;
 
-    if(adcItem()){
+    if (adcItem()) {
         preco++;
     }
 }
--- pagamento/script.js
+++ pagamento/script.js
@@ -3,107 +3,107 @@ var pix = document.getElementById("pix");
 var cartao = document.getElementById("cartao");
 
 
-pix.addEventListener("click", function(){
+pix.addEventListener("click", function () {
 
     var container = document.getElementById("container");
     var container2 = document.getElementById("container2");
 
-    if(container.style.display === "none" || container.style.display === ""){
+    if (container.style.display === "none" || container.style.display === "") {
         container.style.display = "block"
         container2.style.display = "none"
         console.log("container.style.display");
-        
-    }else{
+
+    } else {
         container.style.display = "none";
     }
 })
 
-cartao.addEventListener("click", function(){
+cartao.addEventListener("click", function () {
 
     var container2 = document.getElementById("container2");
     var container = document.getElementById("container");
 
 
-    if(container2.style.display === "none" || container2.style.display === ""){
+    if (container2.style.display === "none" || container2.style.display === "") {
         container2.style.display = "block"
         container.style.display = "none"
         console.log("container.style.display");
-    }else{
+    } else {
         container2.style.display = "none";
     }
 })
- const nome = document.getElementById("fname")
- const telefone = document.getElementById("lnum")
- const endereco = document.getElementById("endereco")
- const complemento = document.getElementById("complemento")
- const titularCartao = document.getElementById("tcartao")
- const numeroCartao = document.getElementById("ncartao")
- const cvv = document.getElementById("cvv")
- const mes = document.getElementById("mes")
- const ano = document.getElementById("ano")
- const pixId = document.getElementById("pixId");
+const nome = document.getElementById("fname")
+const telefone = document.getElementById("lnum")
+const endereco = document.getElementById("endereco")
+const complemento = document.getElementById("complemento")
+const titularCartao = document.getElementById("tcartao")
+const numeroCartao = document.getElementById("ncartao")
+const cvv = document.getElementById("cvv")
+const mes = document.getElementById("mes")
+const ano = document.getElementById("ano")
+const pixId = document.getElementById("pixId");
 
 
- console.log(botao);
- console.log(nomee);
+console.log(botao);
+console.log(nomee);
 
- var nomee = nome.value;
+var nomee = nome.value;
 
-function verifica(){
+function verifica() {
 
-    if(notEmpty(nome)  && notEmpty(telefone)  && notEmpty(endereco)  && notEmpty(titularCartao)
-    && notEmpty(numeroCartao) && notEmpty(cvv) ){
-            document.getElementById("botao").disabled = false;
-    }
-    else{
-        botao.disabled = true;
-    }  
-}
-
-function verificaPix(){
-
-    if(notEmpty(nome)  && notEmpty(telefone)  && notEmpty(endereco)  && notEmpty(pixId)){
+    if (notEmpty(nome) && notEmpty(telefone) && notEmpty(endereco) && notEmpty(titularCartao)
+        && notEmpty(numeroCartao) && notEmpty(cvv)) {
         document.getElementById("botao").disabled = false;
     }
-    else{
+    else {
         botao.disabled = true;
-    } 
+    }
+}
+
+function verificaPix() {
+
+    if (notEmpty(nome) && notEmpty(telefone) && notEmpty(endereco) && notEmpty(pixId)) {
+        document.getElementById("botao").disabled = false;
+    }
+    else {
+        botao.disabled = true;
+    }
 
 }
 
-function notEmpty (componente){
-    return componente != null && componente.value != undefined && componente.value != null && componente.value != '' ;
+function notEmpty(componente) {
+    return componente != null && componente.value != undefined && componente.value != null && componente.value != '';
 }
 
 
 // Apenas letras  
-nome.addEventListener("keypress", function(e) {
+nome.addEventListener("keypress", function (e) {
     var keyCode = (e.keyCode ? e.keyCode : e.which);
-  
-  if (keyCode > 47 && keyCode < 58) {
-    e.preventDefault();
-  }
+
+    if (keyCode > 47 && keyCode < 58) {
+        e.preventDefault();
+    }
 });
-titularCartao.addEventListener("keypress", function(e) {
+titularCartao.addEventListener("keypress", function (e) {
     var keyCode = (e.keyCode ? e.keyCode : e.which);
-  
-  if (keyCode > 47 && keyCode < 58) {
-    e.preventDefault();
-  }
+
+    if (keyCode > 47 && keyCode < 58) {
+        e.preventDefault();
+    }
 });
 
 const pagamento = document.getElementById("pagamento");
 pagamento.addEventListener("mouseover", (event) => {
     event.target.style.color = "orange";
-  
+
     setTimeout(() => {
-      event.target.style.color = "";
+        event.target.style.color = "";
     }, 200);
-  }, false);
+}, false);
 
 
-  
-  mostrarProdutos = function(dadosProduto){
+
+mostrarProdutos = function (dadosProduto) {
 
     document.querySelector('.itemPagamento').innerHTML += `
    
@@ -112,43 +112,43 @@ pagamento.addEventListener("mouseover", (event) => {
             <img class="card-img-top" src="../home-page/${dadosProduto.imagePath}" alt="Card image cap">                
             <div class="card-body">
                 <h5 class="card-title">${dadosProduto.nomeProduto}</h5>
-                <p class="card-text">${dadosProduto.descricao }</p>
+                <p class="card-text">${dadosProduto.descricao}</p>
             </div>               
         </div>
       </td>`;
-                 
+
 }
 
-carrega = function(){
+carrega = function () {
     let itensCarrinho = getParameterByName("carrinho");
     let itensCarrinhoArray = itensCarrinho.split(',');
 
     let xhr = new XMLHttpRequest();
     let url = "http://localhost:8080/api/produtos/findAll";
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (xhr.readyState == XMLHttpRequest.DONE) {
-     
+
             dadosProdutos = JSON.parse(xhr.responseText);
-            if(dadosProdutos.length> 0){
-                for(let i=0; i<dadosProdutos.length; i++){    
-                    if(itensCarrinhoArray.indexOf(dadosProdutos[i].id.toString()) != -1){
+            if (dadosProdutos.length > 0) {
+                for (let i = 0; i < dadosProdutos.length; i++) {
+                    if (itensCarrinhoArray.indexOf(dadosProdutos[i].id.toString()) != -1) {
                         mostrarProdutos(dadosProdutos[i]);
-                    }              
-                   
-                }               
+                    }
+
+                }
             }
-            else{
+            else {
                 alert('Produto não encontrado')
             }
         }
 
 
     }
-    xhr.oneerror=
-    xhr.open('GET',url, true);
+    xhr.oneerror =
+        xhr.open('GET', url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send();
-    
+
 }
 
 getParameterByName = function (name, url = window.location.href) {
